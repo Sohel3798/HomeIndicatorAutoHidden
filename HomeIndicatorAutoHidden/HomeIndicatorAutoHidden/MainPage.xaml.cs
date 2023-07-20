081c1cf4fd4311923139f848dc6b7d3885733c87
Sohel <>
2023-07-20 14:18:10 +0530

    init commit

---
+++ HomeIndicatorAutoHidden/HomeIndicatorAutoHidden/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace HomeIndicatorAutoHidden;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+        On<iOS>().SetPrefersHomeIndicatorAutoHidden(!On<iOS>().PrefersHomeIndicatorAutoHidden());
+    }
+}
+
+
